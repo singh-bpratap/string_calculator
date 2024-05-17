d6d149cd4c94dbf3339ce852c1f7ffcd95cf3a95
Bhanu Pratap <singh.bpratap86@gmail.com>
2024-05-17 17:43:44 +0530

    Changes for new line as delimiter and invalid inputs for negative values

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,31 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1, 2")).to eq(3)
       end
     end
+
+    context "new line separated numbers in input should be accepted" do
+      it "returns sum of numbers" do
+        expect(StringCalculator.add("1\n2")).to eq(3)
+        expect(StringCalculator.add("1\n 2")).to eq(3)
+        expect(StringCalculator.add("1\n,2")).to eq(3)
+        expect(StringCalculator.add("1\n, 2")).to eq(3)
+        expect(StringCalculator.add("1,\n2")).to eq(3)
+        expect(StringCalculator.add("1,\n 2")).to eq(3)
+      end
+
+      it "returns error message if new line is at the end of string" do
+        expect(StringCalculator.add("1\n")).to eq('Invalid input')
+      end
+    end
+
+    context "negative numbers in input should not be accepted" do
+      it "returns error message" do
+        expect(StringCalculator.add("1,-2")).to include("Invalid inputs")
+      end
+
+      it "returns error message with invalid number(s)" do
+        expect(StringCalculator.add("1,-2")).to eq("Invalid inputs: -2")
+        expect(StringCalculator.add("1, -2, -3")).to eq("Invalid inputs: -2, -3")
+      end
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,17 +1,33 @@
 # require './string_calculator'
 
 class StringCalculator
-  attr_accessor :numbers
+  attr_accessor :string
 
   def self.add(string)
     new(string).add
   end
 
   def initialize(string)
-    @numbers = string.to_s.split(/\s*,\s*/).map(&:to_i)
+    @string = string
   end
 
   def add
+    return "Invalid input" if string.to_s[-1] == "\n"
+    return "Invalid inputs: #{invalid_numbers.join(', ')}" if invalid_numbers.any?
+
     numbers.sum
+  end
+
+  private
+
+  def numbers
+    @numbers ||= string
+                 .to_s
+                 .split(/\s*[,\n]+\s*/)
+                 .map(&:to_i)
+  end
+
+  def invalid_numbers
+    numbers.select { |n| n < 0 }
   end
 end
