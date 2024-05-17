ba515bddc91270d174b344b1dc0b55aa45fdf109
Bhanu Pratap <singh.bpratap86@gmail.com>
2024-05-17 16:01:40 +0530

    Add method and a few basic test cases

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,10 +2,41 @@ require 'rspec'
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  describe "#add" do
+  describe ".add" do
     it "returns true if add method is defined" do
-      calculator = StringCalculator.new
-      expect(calculator.public_methods.include?(:add)).to be_truthy
+      expect(StringCalculator.methods.include?(:add)).to be_truthy
+    end
+
+    it "returns true if allow to handle an any amount of numbers in arguments" do
+      begin
+        expect{StringCalculator.add("1")}.not_to raise_error
+        expect{StringCalculator.add("1", "2")}.not_to raise_error
+      rescue RSpec::Expectations::ExpectationNotMetError => e
+        expect(e.message).not_to include 'ArgumentError'
+      end
+    end
+
+    context "empty string input" do
+      it "returns 0" do
+        expect(StringCalculator.add("")).to eq(0)
+        expect(StringCalculator.add(" ")).to eq(0)
+        expect(StringCalculator.add(",")).to eq(0)
+        expect(StringCalculator.add(nil)).to eq(0)
+      end
+    end
+
+    context "empty array input" do
+      it "returns 0" do
+        expect(StringCalculator.add([])).to eq(0)
+      end
+    end
+
+    context "array with empty elements input" do
+      it "returns 0" do
+        expect(StringCalculator.add([nil])).to eq(0)
+        expect(StringCalculator.add([""])).to eq(0)
+        expect(StringCalculator.add([nil, ""])).to eq(0)
+      end
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,20 @@
 # require './string_calculator'
 
 class StringCalculator
+  attr_accessor :numbers
+
+  def self.add(*args)
+    new(args).add
+  end
+
+  def initialize(*args)
+    @numbers = [*args]
+               .flatten
+               .compact
+               .map{|element| element.to_i}
+  end
+
+  def add
+    numbers.sum
+  end
 end
